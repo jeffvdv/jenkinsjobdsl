c1bb59c1f86e5235af733555151f6acb33530303
jeffvdv <jeffvdv@inuits.eu>
2018-06-27 12:19:06 +0200

    first commit

---
+++ aa_init.groovy
@@ -0,0 +1,2 @@
+/* Init */
+folder("puppetry")
---
+++ hosting.groovy
@@ -0,0 +1,42 @@
+import org.yaml.snakeyaml.Yaml
+
+def project = 'hosting'
+folder("${project}")
+
+configFile = readFileFromWorkspace('cfg/hosting.yaml')
+
+def map
+Yaml configFileYaml = new Yaml()
+map = configFileYaml.load(configFile)
+
+map.each() { p ->
+    def projectname = p.project
+    def gitlocation = p.gitlocation
+    def environment = p.environment
+    def deployServer = p.deployServer
+    def branch = p.branch
+    def deployLocation = p.deployLocation
+
+    job("${project}/${projectname}") {
+        logRotator {
+            numToKeep(5)
+            artifactNumToKeep(10)
+        }
+
+        blockOn("${project}/${projectname}.*") {
+            blockLevel('GLOBAL')
+            scanQueueFor('ALL')
+        }
+
+        scm {
+            git("${gitlocation}", "${branch}")
+            }
+        }
+        triggers {
+            scm('H/5 * * * *')
+        }
+
+        steps {
+//            shell("ssh -o StrictHostKeyChecking=no www-data@${deployServer} \"cd ${deployLocation} && git fetch && git reset --hard\"")
+        }
+      }
